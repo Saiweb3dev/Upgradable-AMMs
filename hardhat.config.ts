8c1fcb00d9f7574f72b37135481c143ae3e99171
saik434455@gmail.com <saik434455@gmail.com>
2024-10-14 19:40:03 +0530

    uniswap v3 installed with some file changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,24 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
 
 export default config;
