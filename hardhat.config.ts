a107a8fddf21b3b00e7351c34d2117679fc3aec2
saik434455@gmail.com <saik434455@gmail.com>
2024-10-14 21:03:40 +0530

    pkg and new config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,66 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: "0.7.6",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
     ],
   },
   networks: {
     hardhat: {
+      forking: {
+        url: process.env.MAINNET_RPC_URL || "",
+        enabled: process.env.FORKING_ENABLED === "true",
+      },
       chainId: 1337,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: process.env.MAINNET_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
   },
 };
 
